056212d89599857cdf939f038acff0cf0afb4e16
jcopelenao7 <jess.opelena1575@gmail.com>
2025-09-23 15:37:38 +0800

    - about us, error, icon, index fix

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -19,67 +19,67 @@ function AboutUs() {
       </p>
 
       <div className="row developer-cards">
-        {/* Developer 1 */}
+        {/* Person1 */}
         <div className="col-md-4">
           <div className="card eternal-card">
             <img
               src={JSimg}
               className="card-img-top dev-img"
-              alt="Developer 1"
+              alt="Person1"
             />
             <div className="card-body">
               <h5 className="card-title">Jesiah Opeleña</h5>
               <p className="card-text">
-                She's a 10, turning 11.
+                object
               </p>
               <div className="dev-links">
-                <a href="#" target="_blank" rel="noreferrer">GitHub</a>
-                <a href="#" target="_blank" rel="noreferrer">LinkedIn</a>
-                <a href="#" target="_blank" rel="noreferrer">YouTube</a>
+                <a href="https://github.com/jcopelenao7" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/jesiah-opelena-226157303/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://www.youtube.com/@jcopelena" target="_blank" rel="noreferrer">YouTube</a>
               </div>
             </div>
           </div>
         </div>
 
-        {/* Developer 2 */}
+        {/* Person2 */}
         <div className="col-md-4">
           <div className="card eternal-card">
             <img
               src={GDimg}
               className="card-img-top dev-img"
-              alt="Developer 2"
+              alt="Person2"
             />
             <div className="card-body">
               <h5 className="card-title">Godffrey Paul Sermon</h5>
               <p className="card-text">
-                I can't clear MoC.
+                immovable object
               </p>
               <div className="dev-links">
-                <a href="#" target="_blank" rel="noreferrer">GitHub</a>
-                <a href="#" target="_blank" rel="noreferrer">LinkedIn</a>
-                <a href="#" target="_blank" rel="noreferrer">YouTube</a>
+                <a href="https://github.com/gdznd" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/gdznd/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://www.youtube.com/@godffreypaulsermon4123" target="_blank" rel="noreferrer">YouTube</a>
               </div>
             </div>
           </div>
         </div>
 
-        {/* Developer 3 */}
+        {/* Person3 */}
         <div className="col-md-4">
           <div className="card eternal-card">
             <img
               src={JCimg}
               className="card-img-top dev-img"
-              alt="Developer 3"
+              alt="Person3"
             />
             <div className="card-body">
               <h5 className="card-title">John Carl Velos</h5>
               <p className="card-text">
-                He who is known as JC just like our homie J-
+                the man who can't be moved.
               </p>
               <div className="dev-links">
-                <a href="#" target="_blank" rel="noreferrer">GitHub</a>
-                <a href="#" target="_blank" rel="noreferrer">LinkedIn</a>
-                <a href="#" target="_blank" rel="noreferrer">YouTube</a>
+                <a href="https://github.com/jcvelos" target="_blank" rel="noreferrer">GitHub</a>
+                <a href="https://www.linkedin.com/in/john-carl-velos-127a21351/" target="_blank" rel="noreferrer">LinkedIn</a>
+                <a href="https://github.com/jcvelos" target="_blank" rel="noreferrer">YouTube</a>
               </div>
             </div>
           </div>
